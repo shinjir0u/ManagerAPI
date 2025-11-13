b386e79d446171d2e3233f54ec5104ecba3827a0
Shinjirou <htooagwai9@gmail.com>
2025-11-13 11:40:21 +0630

    Implement Authorization Documentation with Swagger

--- web/src/main/java/com/repository/manager/web/WebApplication.java
+++ web/src/main/java/com/repository/manager/web/WebApplication.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Import;
 
 import com.repository.manager.web.security.Config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Github Repository Management API", version = "1.0", description = "API for managing github repositories"))
 @SpringBootApplication(scanBasePackages = "com.repository.manager")
 @Import(Config.class)
 public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-
 }
--- web/src/main/java/com/repository/manager/web/controller/AuthorizationController.java
+++ web/src/main/java/com/repository/manager/web/controller/AuthorizationController.java
@@ -13,20 +13,33 @@ import com.repository.manager.core.model.AuthorizationApiResponse;
 import com.repository.manager.service.authorization.AuthorizationService;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/github")
+@Tag(name = "Authorization", description = "Authorize the App to Github Account")
+@SecurityRequirement(name = "AppAuthToken")
 public class AuthorizationController {
 	Dotenv dotenv = Dotenv.load();
 
 	@Autowired
 	private AuthorizationService authorizationService;
 
+	@Operation(summary = "Redirects to Github Page to authorize the API")
+	@ApiResponse(responseCode = "200", description = "This endpoint calls the Github API for authoriaztion "
+			+ ", after which Github API will return the access token.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthorizationApiResponse.class)))
 	@GetMapping("/authorize")
 	RedirectView redirectUserToAuthorizationPage() {
 		return authorizationService.redirectUser();
 	}
 
+	@Hidden
 	@GetMapping("/callback")
 	AuthorizationApiResponse getUserAccessTokenToOAuthApp(@RequestParam String code, @RequestParam String state)
 			throws IOException {
