7985a737719d5398688dd14255bf3cb5a7f30789
Shinjirou <htooagwai9@gmail.com>
2025-12-01 20:04:27 +0630

    Fix bug: jasper service isn't mocked in test

--- web/src/test/java/com/repository/manager/web/RepositoryControllerTest.java
+++ web/src/test/java/com/repository/manager/web/RepositoryControllerTest.java
@@ -26,6 +26,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.repository.manager.core.model.RepositoryResponse;
+import com.repository.manager.service.jasper.JasperService;
 import com.repository.manager.service.repository.RepositoryService;
 import com.repository.manager.web.controller.RepositoryController;
 
@@ -34,6 +35,9 @@ import com.repository.manager.web.controller.RepositoryController;
 public class RepositoryControllerTest {
 	@MockitoBean
 	private RepositoryService repositoryService;
+
+	@MockitoBean
+	private JasperService jasperService;
 
 	@Autowired
 	private MockMvc mockMvc;
