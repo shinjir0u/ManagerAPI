7a66ef99c48ad9c562b72ba3965a3d529afce7d2
Shinjirou <htooagwai9@gmail.com>
2025-12-01 20:09:35 +0630

    Refactor database with local postgresql

--- web/src/main/java/com/repository/manager/web/WebApplication.java
+++ web/src/main/java/com/repository/manager/web/WebApplication.java
@@ -1,13 +1,8 @@
 package com.repository.manager.web;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import com.repository.manager.security.config.SecurityConfig;
 
@@ -21,10 +16,5 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
-	}
-
-	@Bean
-	DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
 	}
 }
